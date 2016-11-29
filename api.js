ffc3e237f9f585684b1032aecdfbb6593c84f54d
Rooted1 <ruthobey@gmail.com>
2016-11-29 19:48:45 +0100

    first api commit

---
+++ api.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+ var options = {
+        host: 'api.forismatic.com',
+        path: '/api/1.0/?method=getQuote&lang=en&format=json',
+        method: 'GET',
+        headers: {'user-agent': 'node.js'}
+    };
+
+http.get(options, function (response) {
+  response.setEncoding('utf8')
+  response.on('data', (quoteData) => {
+    const quote = JSON.parse(quoteData);
+    const quoteText = quote.quoteText;
+    const quoteAuthor = quote.quoteAuthor
+    console.log(quoteText );
+  });
+ // response.on('error', console.error)
+}).on('error', console.error)
+  
